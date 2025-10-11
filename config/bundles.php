e0facfdbdc25ff06222a818ec9a4ff9cfd7f9eb5
damworks <larry.chiarello@gmail.com>
2025-10-11 11:23:09 +0200

    Add PerspectiveEditorBundle to bundles and update composer files

--- config/bundles.php
+++ config/bundles.php
@@ -2,9 +2,11 @@
 
 use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
 use Pimcore\Bundle\DataImporterBundle\PimcoreDataImporterBundle;
+use Pimcore\Bundle\PerspectiveEditorBundle\PimcorePerspectiveEditorBundle;
 
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     PimcoreDataHubBundle::class => ['all' => true],
     PimcoreDataImporterBundle::class => ['all' => true],
+    PimcorePerspectiveEditorBundle::class => ['all' => true],
 ];
