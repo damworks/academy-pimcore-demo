2f5ec07cf994bb749805543d488cb466474779ac
damworks <larry.chiarello@gmail.com>
2025-10-12 10:03:02 +0200

    Add image and image_preview fields to News model and create news_preview thumbnail configuration

--- src/Model/DataObject/News.php
+++ src/Model/DataObject/News.php
@@ -23,6 +23,8 @@ class News extends \Pimcore\Model\DataObject\News implements JsonSerializable
             'description' => $this->getDescription(),
             //'content' => $this->getContent(),
             'date' => $this->getDate() ? $this->getDate()->format('Y-m-d') : null,
+            'image' => $this->getImage() ? $this->getImage()->getFrontendPath() : null,
+            'image_preview' => $this->getImage() ? $this->getImage()->getThumbnail('news_preview')->getFrontendPath() : null,
             'categories' => $categories,
             //'categories' => $this->getCategories() ? array_map(fn($category) => $category->getName(), $this->getCategories()) : [],
             //'published' => $this->getPublished(),
