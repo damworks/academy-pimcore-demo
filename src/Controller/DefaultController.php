453d08415b1a1277f786f1560518968816ddaca8
damworks <larry.chiarello@gmail.com>
2025-10-17 15:21:27 +0200

    Add test route for fetching a specific blog post and mapping data

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controller;
 
+use App\DataMapper\Areas\LatestBlogPostsDataMapper;
 use Pimcore\Bundle\AdminBundle\Controller\Admin\LoginController;
 use Pimcore\Controller\FrontendController;
+use Pimcore\Model\DataObject\BlogPost;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends FrontendController
 {
+
+    #[Route('/testing-url', name: 'testing_url')]
+    public function test(Request $request)
+    {
+        $test = BlogPost::getById(403);
+        //dd($test);
+        dd((new LatestBlogPostsDataMapper($test))->toArray($request));
+    }
     public function defaultAction(Request $request): Response
     {
         return $this->render('default/default.html.twig');
